d5031a733f5b8f96f42c44dfc78642e42af48fcf
jshin1223 <jshin1223@gmail.com>
2019-08-30 17:04:44 -0500

    update

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  namespace :api do
+    get '/query_params_url' => 'params_examples#query_params_action'
+    get '/url_segment_params_url/:wildcard/butter/:pickle_rick/philosophy' => 'params_examples#url_segment_params_action'
+    post '/body_params' => 'params_examples#body_params'
+  end
+end
